33976ad86ba77da64f3888998a539ea0f2e289c6
Koji Miyazato <viercc@gmail.com>
2021-09-20 20:20:06 +0900

    Use the latest subtype-containers (with INLINABLE)

--- benchmark_results/regressions.py
+++ benchmark_results/regressions.py
@@ -1,5 +1,10 @@
+import sys
 import pandas as pd
 import numpy as np
+
+if len(sys.argv)<3:
+    sys.exit("Needs at least 2 file to compare")
+filenames = sys.argv[1:]
 
 schema = [
   ('Workload', 'category'),
@@ -21,21 +26,27 @@ def read_csv(fname):
         return pd.read_csv(fd, sep='[/,]', engine='python',
             header=None, names=names, dtype=dtype, index_col=[0,1,2,3])
 
-baseline_time = read_csv('baseline.csv').Time
-after_time = read_csv('after.csv').Time
+all_timedata = []
+for f in filenames:
+    data = read_csv(f).Time
+    control_time = data['URI', 'Trie', :, 'reverse'][0]
+    ans = data[:, 'ADFA'] # / control_time
+    all_timedata.append(ans)
 
-def normalize(df):
-    t_base=df['URI', 'Trie', :, 'reverse'][0]
-    ans = df[:, 'ADFA'] / t_base
-    return ans
+all_time_ratio = {}
+is_regression = pd.Series(False, all_timedata[0].index)
+is_improvement = pd.Series(False, all_timedata[0].index)
 
-baseline_control_time = baseline_time['URI', 'Trie', :, 'reverse'][0]
-after_control_time = after_time['URI', 'Trie', :, 'reverse'][0]
-control_ratio = after_control_time / baseline_control_time
+for i in range(1,len(filenames)):
+    time_ratio = all_timedata[i] / all_timedata[0]
+    all_time_ratio[filenames[i]] = time_ratio
+    is_regression |= time_ratio > 1.1
+    is_improvement |= time_ratio < 0.9
 
-time_ratio = after_time[:, 'ADFA'] / baseline_time[:, 'ADFA'] / control_ratio
+all_time_ratio = pd.DataFrame(all_time_ratio)
 
 print("Regressions:")
-print(time_ratio[time_ratio > 1.1])
+print(all_time_ratio[is_regression])
 print("Improvements:")
-print(time_ratio[time_ratio < 0.9])
+print(all_time_ratio[is_improvement])
+
